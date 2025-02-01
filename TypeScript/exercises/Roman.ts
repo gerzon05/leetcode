b3e1331d37e1b29c8daa127853f702b260406a22
Gerzon05 <gerzonarletrangel@gmail.com>
2025-01-31 22:11:54 -0500

    Add exercise of roman numeral typescript

--- TypeScript/exercises/Roman.ts
+++ TypeScript/exercises/Roman.ts
@@ -17,8 +17,8 @@
 
 // Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 
-// I can be placed before V (5) and X (10) to make 4 and 9. 
-// X can be placed before L (50) and C (100) to make 40 and 90. 
+// I can be placed before V (5) and X (10) to make 4 and 9.
+// X can be placed before L (50) and C (100) to make 40 and 90.
 // C can be placed before D (500) and M (1000) to make 400 and 900.
 // Given a roman numeral, convert it to an integer.
 //
@@ -37,8 +37,8 @@
 
 // Los números romanos suelen escribirse de mayor a menor, de izquierda a derecha. Sin embargo, el número cuatro no es IIII. En su lugar, el número cuatro se escribe IV. Como el uno está antes del cinco, lo restamos y obtenemos cuatro. El mismo principio se aplica al número nueve, que se escribe IX. Hay seis casos en los que se utiliza la resta:
 
-// I puede colocarse antes de V (5) y X (10) para formar 4 y 9. 
-// X puede anteponerse a L (50) y C (100) para formar 40 y 90. 
+// I puede colocarse antes de V (5) y X (10) para formar 4 y 9.
+// X puede anteponerse a L (50) y C (100) para formar 40 y 90.
 // C puede colocarse antes de D (500) y M (1000) para formar 400 y 900.
 // Dado un número romano, conviértelo en un número entero.
 //
@@ -49,3 +49,46 @@
 // Explanation: III = 3.
 //
 // =============================================================================
+
+const romanNumerals: Record<string, number> = {
+  "I": 1,
+  "V": 5,
+  "X": 10,
+  "L": 50,
+  "C": 100,
+  "D": 500,
+  "M": 1000
+}
+
+function romanToInt(s: string): number {
+  const letterRomanArrays: string[] = s.split("")
+  let Numbers: number[] = []
+  let total: number[] = []
+
+  for (const number of letterRomanArrays) {
+    const romanNumeralInteger = romanNumerals[number];
+
+    if (!romanNumeralInteger) continue
+
+    Numbers.push(romanNumeralInteger)
+  }
+
+  for (let i = 0; i < Numbers.length; i++) {
+    if (
+      Numbers[i] > Numbers[i + 1] ||
+      Numbers[i] === Numbers[i + 1]
+    ) {
+      total.push(Numbers[i]);
+    } else if (Numbers[i] < Numbers[i + 1]) {
+      const result = Numbers[i + 1] - Numbers[i];
+      total.push(result);
+      i++;
+    } else {
+      total.push(Numbers[i]);
+    }
+  }
+
+  return total.reduce((acc, value) => acc + value, 0);
+};
+
+console.log(romanToInt("IV"))
