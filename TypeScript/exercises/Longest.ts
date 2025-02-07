03da6569716ce33e6476d305ce34e97962f14708
Gerzon05 <gerzonarletrangel@gmail.com>
2025-02-06 21:39:32 -0500

    Add exercise to find the longest common prefix in TypeScript

---
+++ TypeScript/exercises/Longest.ts
@@ -0,0 +1,29 @@
+// Longest Common Prefix
+// Easy
+// Topics
+// Companies
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+// Example 1:
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+// Español
+
+// Prefijo común más largo
+// Fácil
+// Temas
+// Empresas
+// Escriba una función para encontrar la cadena de prefijo común más larga entre una matriz de cadenas.
+
+// Si no hay prefijo común, devuelve una cadena vacía «».
+
+function longestCommonPrefix(strs: string[]): string {
+    return ""
+};
