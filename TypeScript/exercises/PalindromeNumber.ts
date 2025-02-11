9889da13ce3ea6a66edc361e10f1d7df1344b2b7
Gerzon05 <gerzonarletrangel@gmail.com>
2025-02-10 20:32:25 -0500

    Add exercise to check if a number is a palindrome in TypeScript

--- TypeScript/exercises/PalindromeNumber.ts
+++ TypeScript/exercises/PalindromeNumber.ts
@@ -17,3 +17,20 @@
 // Output: true
 // Explanation: 121 reads as 121 from left to right and from right to left.
 //
+
+function isPalindrome(x: number): boolean {
+  const number: number = x
+  const Arraynumber = number.toString().split("")
+  const ArraysRever: string[] = []
+  for (let index = Arraynumber.length-1; index >= 0; index--) {
+    ArraysRever.push(Arraynumber[index])
+  }
+  const palindromeNumber: number = Number(ArraysRever.join(""))
+
+  if (x === palindromeNumber) {
+    return true
+  }
+  return false
+};
+
+console.log(isPalindrome(121))
