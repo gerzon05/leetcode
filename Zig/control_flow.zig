be9a29f4612ebbd3971fd8fe4a8a852cf0066fa8
Gerzon05 <gerzonarletrangel@gmail.com>
2025-02-27 21:33:28 -0500

    Learn Arrays and Slices

--- Zig/control_flow.zig
+++ Zig/control_flow.zig
@@ -2,9 +2,18 @@ const std = @import("std");
 pub fn main() void {
     const Number = 13;
 
+    // const arrayNumber = [10]u8{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; No puede cambiar su valor
+    var arrayNumber = [10]u8{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    const newArray = arrayNumber[0..arrayNumber.len]; // no importa el tipo depende del tipo de arrays anterior
+
+    newArray[2] = 34;
+
+    std.debug.print("{d}\n", .{newArray[2]});
+
     if (Number >= 18) {
-        std.debug.print("Eres Mayor de edad", .{});
+        std.debug.print("Eres Mayor de edad\n", .{});
     } else {
-        std.debug.print("Eres menor de edad", .{});
+        std.debug.print("Eres menor de edad\n", .{});
     }
 }
