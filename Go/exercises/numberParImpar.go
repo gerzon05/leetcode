07b4c8b625e3e1b20f98da9b25af334b14f87e73
gerzon05 <gerzonarletrangel@gmail.com>
2025-05-12 11:04:20 -0500

    Exercise(Go): identify odd and even numbers

---
+++ Go/exercises/numberParImpar.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ParImpar(15)
+	ParImpar(30)
+}
+
+func ParImpar(num int) {
+	fmt.Printf("=================  Numeros Enteros Hasta %d ==========================\n", num)
+	for number := 1; number <= num; number++ {
+		if number%2 == 0 {
+			fmt.Println("Numero Par: ", number)
+		} else if number%2 == 1 {
+			fmt.Println("Numero Impar: ", number)
+		} else {
+			fmt.Println("Este Numero no es Par ni Impar: ", number)
+		}
+	}
+	fmt.Println("======================================================================")
+}
