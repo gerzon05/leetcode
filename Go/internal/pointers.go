d4eb2469b0db67d6d891aa9f02187acae4a68e0e
gerzon05 <gerzonarletrangel@gmail.com>
2025-05-19 17:44:43 -0500

    learn(Go): learned about value by reference

--- Go/internal/pointers.go
+++ Go/internal/pointers.go
@@ -11,8 +11,8 @@ func RunPointer() {
 	a := &i // guardar la direccion de memoria de la vatiable i
 	p := *a // guardar el valor a donde apunta esa direccion de memoria a
 
-	fmt.Println("Direccion de memoria &: ", a)
-	fmt.Println("Apuntado a la direccion de memoria *: ", p)
+	fmt.Println("Direccion de memoria &: ", a)               //
+	fmt.Println("Apuntado a la direccion de memoria *: ", p) // 10
 
 	*a = 20 // cambiar el valor de la direccion de memoria que se esta referenciando
 
@@ -20,4 +20,32 @@ func RunPointer() {
 	fmt.Println("Apuntado a la direccion de memoria desde p: ", p)
 	fmt.Println("Apuntado a la direccion de memoria desde i: ", i)
 
+	// Paso por Valor - Paso por referencia
+	fmt.Println("Paso por valor")
+
+	valueIncrement := 3
+
+	fmt.Println("Valor a Incrementar: ", valueIncrement)
+
+	incrementValue(valueIncrement) // valor por parametro crea una copia dentro de la funcion
+
+	fmt.Println("Valor a Incrementar: ", valueIncrement)
+
+	fmt.Println("Paso por Referencia")
+
+	valueReference := 3
+
+	fmt.Println("Valor a Referenciar: ", valueReference)
+
+	incrementValueReference(&valueReference) // Pasamos la direccion de memoria
+
+	fmt.Println("Valor a Refenrenciar: ", valueReference)
+
+}
+
+func incrementValue(a int) {
+	a++
+}
+func incrementValueReference(a *int) { // recibe la posicion de memoria sin hacer una copia del valor
+	*a++ // apunta a la direccion de memoria (valor) y le aumenta 1
 }
